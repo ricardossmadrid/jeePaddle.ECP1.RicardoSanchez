af3f765b7877fc56dd175b7297167d4f3efe2ee7
ricardoss <ricardoss@ricardoss-SATELLITE-S50D-B>
2016-03-28 02:10:36 +0200

    Se añade test, se comprueba el momento de expiración del token

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -17,5 +17,17 @@ public class TokenTest {
         Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
     }
+    
+    @Test
+    public void testCreationExpirationTime() {
+    	User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        Token token = new Token(user);
+        Calendar auxiliarTime = Calendar.getInstance();
+        auxiliarTime.add(Calendar.HOUR, 1);
+        auxiliarTime.add(Calendar.SECOND, 1);
+        assertTrue(token.getExpirationTime().before(auxiliarTime));
+        auxiliarTime.add(Calendar.SECOND, -2);
+        assertTrue(token.getExpirationTime().after(auxiliarTime));
+    }
 
 }
